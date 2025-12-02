9731d156c2655265759599c51ea05b8ad7456245
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2025-12-02 14:47:12 -0500

    various updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ export const routes: Routes = [
     { path: '', component: HomepageComponent },  // Default route (Home)
     { path: 'about', component: AboutComponent }, // Route for the About page
     { path: 'work', component: WorkComponent}, // Route for the Projects and Work page
-    { path: '**', redirectTo: '', pathMatch: 'full' }  // Fallback to Home if no route is found
-];
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Fallback to Home if no route is found
+    ];
 
 export const appRoutingProviders = [provideRouter(routes)];
